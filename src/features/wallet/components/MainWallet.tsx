7034dac2f9543f9b6ff7d4e5764cc6805df88ba3
Rama Aryasuta <ramaaryasutaweihan@gmail.com>
2026-04-23 17:47:10 +0700

    fix (wallet) : reduce font size

--- src/features/wallet/components/MainWallet.tsx
+++ src/features/wallet/components/MainWallet.tsx
@@ -3,7 +3,7 @@ import WalletCard from "../../../components/Card/WalletCard"
 const MainWallet = () => {
     return (
         <div>
-            <h1 className="text-lg text-gray-400 font-medium mb-4">Dompet Utama</h1>
+            <h1 className="text-md text-gray-400 font-medium mb-4">Dompet Utama</h1>
             <div className="flex gap-3 overflow-x-auto scrollbar-hide">
                 <div className="w-8/12 md:w-6/12 lg:w-4/12 flex-shrink-0">
                     <WalletCard name="Mezink" balance={2340050} badge="utama" />
